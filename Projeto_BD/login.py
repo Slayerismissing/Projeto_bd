aff38da00af89d24fe2ee583250d7b6ec19b3480
Slayerismissing <158784753+Slayerismissing@users.noreply.github.com>
2025-11-12 13:28:29 -0300

    teste
    mudanças
    
    Co-Authored-By: Pedro Correia de Araújo Moura <77297069+pedro470@users.noreply.github.com>

--- Projeto_BD/login.py
+++ Projeto_BD/login.py
@@ -1,10 +1,21 @@
 from db import criar_conexao
+import os 
 
 def delete_equipe(id_equipe):
     conn = criar_conexao()
     cursor = conn.cursor()
     query = 'DELETE FROM equipes WHERE id_equipe=%s'
-    return cursor.execute(query, [id_equipe])
+    try:
+       cursor.execute(query, [id_equipe])
+       conn.commit
+       print(f'Equipe com ID {id_equipe} removida com sucesso!')
+       return True
+    except Exception as e:
+        conn.rollback()
+        print(f'Erro ao deletar: {e}')
+        return False
+    finally:
+        conn.close()
 
 def update_equipe(nome, id_equipe, regiao) :
     conn = criar_conexao()
@@ -20,11 +31,35 @@ def tabela():
     cursor.execute(query)
     return cursor.fetchall()
 
-equipes = tabela()
 
+os.system('cls')
+
+equipes = tabela()
+print('ID;\tNome\tRegião;\tInte:\tLíder;\tNome Líder;')
 for i in equipes:
     print(i)
-    
+
+
+
+
+id_equipes = input('Digite o id do time que quer tirar: ')
+
+id_equipes_int = int(id_equipes)
+
+deletar = delete_equipe(id_equipes_int)
+
+
+
+
+
+
+
+
+
+equipes = tabela()
+print('\nTabela após exclusão:')
+for i in equipes:
+    print(i)
 
 
 
