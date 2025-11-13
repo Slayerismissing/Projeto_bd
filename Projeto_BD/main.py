482513464514df6cd94f1af49dd369b204d65cfd
Pedro Correia de Araújo Moura <77297069+pedro470@users.noreply.github.com>
2025-11-12 23:35:16 -0300

    Revert "Merge remote-tracking branch 'upstream/main'"
    This reverts commit 0c0dcfe3fbfb1b1a618c08b203b9b594fc11151d, reversing
    changes made to 48bb10fd198275befa50d56ed5388305dd2a872e.

--- Projeto_BD/main.py
+++ Projeto_BD/main.py
@@ -1,5 +1,18 @@
-from conexão.db import criar_conexao
+from db import criar_conexao
+import interfaces.equipes_interface as interface_equipes
 
-# Fazer aqui a "interface" pro usuário
+conn = criar_conexao()
 
-
+#todo: separar as interfaces do main
+while True:
+    opcao = input('Digite a opção desejada: 1- Inserir equipe, 2- Deletar equipe, 3-') 
+    if opcao == '1':
+        interface_equipes.interface_inserir_equipe()
+    elif opcao == '2':
+        interface_equipes.interface_deletar_equipe()
+    elif opcao == '3':
+        break
+    else:
+        print('Opção inválida. Tente novamente.')
+if conn:
+    conn.close()
